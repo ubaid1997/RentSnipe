e92895c0f11d8b7c4386d44e7ffb08787aa190e2
ubaid1997 <71568162+ubaid1997@users.noreply.github.com>
2022-02-21 18:05:46 -0500

    test2

--- rent.py
+++ rent.py
@@ -3,7 +3,8 @@ from selenium import webdriver
 #%%
 url = 'https://www.districtwestapts.com/floorplans'
 a = os.getcwd() 
-browser = webdriver.Chrome()
+browser = webdriver.Chrome('/Users/ubaidrahim/Desktop/env/chromedriver')
 browser.get(url)
 
 # %%
+d = okay next
